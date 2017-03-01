c920d7f02f2a1fe58d98d70ae3d6dfbf4cee007b
Andreas von Studnitz <avs@integer-net.de>
2017-03-01 16:01:30 +0100

    Show hints for website scope config values

--- Plugin/ConfigFieldPlugin.php
+++ Plugin/ConfigFieldPlugin.php
@@ -2,9 +2,38 @@
 namespace AvS\ScopeHint\Plugin;
 
 use Magento\Config\Model\Config\Structure\Element\Field as Subject;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Store\Api\WebsiteRepositoryInterface;
+use Magento\Store\Api\Data\WebsiteInterface;
 
 class ConfigFieldPlugin
 {
+    const SCOPE_TYPE_WEBSITES = 'websites';
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+    /**
+     * @var WebsiteRepositoryInterface
+     */
+    private $websiteRepository;
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        WebsiteRepositoryInterface $websiteRepository,
+        RequestInterface $request
+    )
+    {
+        $this->scopeConfig = $scopeConfig;
+        $this->websiteRepository = $websiteRepository;
+        $this->request = $request;
+    }
+
     /**
      * @param Subject $subject
      * @param string $result
@@ -12,7 +41,17 @@ class ConfigFieldPlugin
      */
     public function afterGetTooltip(Subject $subject, $result)
     {
-        return $result;
+        $lines = [$result];
+        foreach($this->websiteRepository->getList() as $website) {
+            if ($this->isWebsiteSelected($website)) {
+                continue;
+            }
+
+            if ($scopeLine = $this->getScopeHint($this->getPath($subject), self::SCOPE_TYPE_WEBSITES, $website)) {
+                $lines[] = $scopeLine;
+            }
+        }
+        return implode('<br />', array_filter($lines));
     }
 
     /**
@@ -25,7 +64,43 @@ class ConfigFieldPlugin
         if (strlen(trim($result))) {
             $result .= '<br />';
         }
-        $result .= __('Path: <code>%1</code>', $subject->getPath());
+        $result .= __('Path: <code>%1</code>', $this->getPath($subject));
         return $result;
+    }
+
+    /**
+     * @param Subject $subject
+     * @return string
+     */
+    private function getPath(Subject $subject)
+    {
+        return $path = $subject->getConfigPath() ?: $subject->getPath();
+    }
+
+    /**
+     * @param WebsiteInterface $website
+     * @return bool
+     */
+    private function isWebsiteSelected($website)
+    {
+        return $website->getId() == $this->request->getParam('website');
+    }
+
+    /**
+     * @param $path
+     * @param $scopeType
+     * @param $website
+     * @return \Magento\Framework\Phrase|string
+     */
+    private function getScopeHint($path, $scopeType, $website)
+    {
+        $scopeLine = '';
+        $currentValue = $this->scopeConfig->getValue($path);
+        $scopeValue = $this->scopeConfig->getValue($path, $scopeType, $website->getId());
+        if ($scopeValue != $currentValue) {
+            $scopeLine = __('Website <code>%1</code>: "%2"', $website->getCode(), $scopeValue);
+            return $scopeLine;
+        }
+        return $scopeLine;
     }
 }
