39736758808ce2c8bdd59c659c012c866c72556a
Ernst Vaarties <e.vaarties@xsarus.nl>
2024-12-06 15:20:10 +0000

    No ticket. Config setting to disable display of attribute codes.

--- Plugin/UIScopeLabel.php
+++ Plugin/UIScopeLabel.php
@@ -2,19 +2,27 @@
 namespace AvS\ScopeHint\Plugin;
 
 use Magento\Eav\Api\Data\AttributeInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Stdlib\ArrayManager;
-use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\Eav;
+use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\Eav as EavModifier;
 
 class UIScopeLabel
 {
+    private const CONFIG_SHOW_ATTRIBUTE_CODE = 'dev/debug/show_attribute_code_in_adminhtml';
+
     public function __construct(
         protected ArrayManager $arrayManager,
+        protected ScopeConfigInterface $scopeConfig,
     ) {
     }
 
-    public function afterSetupAttributeMeta(Eav $subject, $result, $attribute)
+    public function afterSetupAttributeMeta(EavModifier $subject, $result, $attribute)
     {
-        $configPath = ltrim(Eav::META_CONFIG_PATH, ArrayManager::DEFAULT_PATH_DELIMITER);
+        if (!$this->scopeConfig->isSetFlag(self::CONFIG_SHOW_ATTRIBUTE_CODE)) {
+            return $result;
+        }
+
+        $configPath = ltrim(EavModifier::META_CONFIG_PATH, ArrayManager::DEFAULT_PATH_DELIMITER);
         $scopeLabel = $this->arrayManager->get(
             $configPath . ArrayManager::DEFAULT_PATH_DELIMITER . 'scopeLabel',
             $result
